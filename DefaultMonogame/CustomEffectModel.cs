d0267cd9bf01baa28e136c418e76f65c51348816
Sam ColvilleRea <S00182360@mail.itsligo.ie>
2020-03-04 11:59:45 +0000

    Basic setup complete

---
+++ DefaultMonogame/CustomEffectModel.cs
@@ -0,0 +1,160 @@
+﻿using Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultMonogame
+{
+    class CustomEffectModel : SimpleModel
+    {
+        public Effect CustomEffect;
+        public Material Material;
+
+        public CustomEffectModel(string assetName, Vector3 position):base("", assetName, position)
+        {
+
+        }
+
+        private void GenerateMeshTag()
+        {
+            foreach (ModelMesh mesh in Model.Meshes)
+            {
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+
+                    if (part.Effect is BasicEffect)
+                    {
+                        MeshTag tag = new MeshTag();
+                        tag.Color = (part.Effect as BasicEffect).DiffuseColor;
+                        tag.Texture = (part.Effect as BasicEffect).Texture;
+                        tag.SpecularPower = (part.Effect as BasicEffect).SpecularPower;
+
+                        part.Tag = tag;
+                    }
+                }
+            }
+        }
+
+        public virtual void CacheEffect()
+        {
+            foreach (ModelMesh mesh in Model.Meshes)
+            {
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+                    if (part.Tag != null)
+                        if ((part.Tag as MeshTag).CachedEffect != null)
+                            (part.Tag as MeshTag).CachedEffect = part.Effect;
+                }
+            }
+        }
+
+        public virtual void RestoreEffect()
+        {
+            foreach (ModelMesh mesh in Model.Meshes)
+            {
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+                    if ((part.Tag as MeshTag).CachedEffect != null)
+                        part.Effect = (part.Tag as MeshTag).CachedEffect;
+                }
+            }
+        }
+
+        public virtual void SetEffectParameter(Effect effect, string paramName, object value)
+        {
+            if (effect.Parameters[paramName] == null)
+                return;
+
+            if (value is Vector3)
+                effect.Parameters[paramName].SetValue((Vector3)value);
+            else if (value is Matrix)
+                effect.Parameters[paramName].SetValue((Matrix)value);
+            else if (value is bool)
+                effect.Parameters[paramName].SetValue((bool)value);
+            else if (value is Texture2D)
+                effect.Parameters[paramName].SetValue((Texture2D)value);
+            else if (value is float)
+                effect.Parameters[paramName].SetValue((float)value);
+            else if (value is int)
+                effect.Parameters[paramName].SetValue((int)value);
+        }
+
+        public virtual void SetModelEffect(Effect effect, bool copyEffect)
+        {
+            foreach (ModelMesh mesh in Model.Meshes)
+            {
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+                    Effect toBeSet = effect;
+
+                    if (copyEffect)
+                        toBeSet = effect.Clone();
+
+                    var tag = (part.Tag as MeshTag);
+
+                    if (tag.Texture != null)
+                    {
+                        SetEffectParameter(toBeSet, "Texture", tag.Texture);
+                        SetEffectParameter(toBeSet, "TextureEnabled", true);
+                    }
+                    else
+                        SetEffectParameter(toBeSet, "TextureEnabled", false);
+
+                    SetEffectParameter(toBeSet, "DiffuseColo", tag.Color);
+                    SetEffectParameter(toBeSet, "SpecularPower", tag.SpecularPower);
+
+                    part.Effect = toBeSet;
+                }
+            }
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+
+            //replace effect that was applied to the model
+            //default is BasicEffect
+            //replace with any custom effect
+            if (Model != null)
+            {
+                GenerateMeshTag();
+
+                if (CustomEffect != null)
+                    foreach (ModelMesh mesh in Model.Meshes)
+                        foreach (ModelMeshPart part in mesh.MeshParts)
+                            part.Effect = CustomEffect;
+            }
+        }
+
+        public override void Draw(Camera camera)
+        {
+            if (CustomEffect != null)
+            {
+                SetModelEffect(CustomEffect, false);
+
+                foreach (ModelMesh mesh in Model.Meshes)
+                {
+                    foreach (ModelMeshPart part in mesh.MeshParts)
+                    {
+                        CustomEffect.Parameters["World"].SetValue(BoneTransforms[mesh.ParentBone.Index] * World);
+                        CustomEffect.Parameters["View"].SetValue(camera.View);
+                        CustomEffect.Parameters["Projection"].SetValue(camera.Projection);
+
+                        if (Material != null)
+                            Material.SetEffectParameters(part.Effect);
+                    }
+
+                    mesh.Draw();
+                }
+            }
+            else
+            {
+                base.Draw(camera);
+            }
+        }
+    }
+}
---
+++ DefaultMonogame/Material.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultMonogame
+{
+    public abstract class Material
+    {
+        public virtual void Update() { }
+        //Call from Draw Method
+        //Pass effect that is applied to the model
+        //Override and set the effect parameters
+        public virtual void SetEffectParameters(Effect effect) { }
+    }
+}
